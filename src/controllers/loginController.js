910b70423a863fbc99b4d89d6006c161fb801db5
Igor Souza <igor_carmo64@hotmail.com>
2022-04-20 16:08:18 -0300

    AgendaExpressMVC_V2

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,65 @@
-exports.index =(req, res) => {
+const Login = require('../models/LoginModel')
+
+exports.index = (req, res) => {
     res.render('login');
+};
+
+exports.register = async function (req, res) {
+
+    try {
+        const login = new Login(req.body);
+        await login.register();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+
+            req.session.save(function () {
+                return res.redirect('index');
+            });
+
+            return;
+        }
+
+            req.flash('success', 'Seu usuário foi criado com sucesso!');
+            req.session.save(function () {
+                return res.redirect('index');
+            });
+
+    } catch (e) {
+        return res.render('404');
+    }
+
+
+};
+
+
+exports.login = async function (req, res) {
+
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+
+            req.session.save(function () {
+                return res.redirect('index');
+            });
+
+            return;
+        }
+
+       
+
+        req.flash('success', 'Você entrou no sistema');
+        req.session.user = login.user;
+            req.session.save(function () {
+                return res.redirect('index');
+            });
+
+    } catch (e) {
+        return res.render('404');
+    }
+
+
 };
